7198ea23c8680e6e068fdbcb878e0112de305d53
drFabio <blackjackdevel@gmail.com>
2018-11-25 13:09:58 +0100

    New: added sandbox and package json

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function App() {
+  return (
+    <h1>Funciona!</h1>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
